d9155861aaadfd7d34decfe06bf2ea36c87e21e7
thierry <tlouis7@bostonk12.org>
2024-03-15 09:42:52 -0400

    Merge remote-tracking branch 'origin/main'

---
+++ Week_13/2024-03-13/about_me.py
@@ -0,0 +1,7 @@
+#my name is thierry
+# it is 2024-03-15
+
+print("the city im from is port-au-prince")
+print("the country im from is haiti")
+print ("im am 15 years old")
+print ("my favorite class is GYM")
